313e4fb04881962d86a5bf4dbb44a7cb110cb254
evelyn-lee <evelynpuiee@gmail.com>
2016-08-06 13:51:26 +1200

    Add project files and folders

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "what is hello";
+document.body.innerHTML = greeter(user);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "what is hello";
+
+document.body.innerHTML = greeter(user);   
